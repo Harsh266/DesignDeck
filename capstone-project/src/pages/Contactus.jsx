2f1c5868ead261e6af201b5c8efbf4ed4fa3e01c
Harsh Vekriya <harshvekariya203@gmail.com>
2025-03-19 23:01:25 +0530

    solve deploy error

--- capstone-project/src/pages/Contactus.jsx
+++ capstone-project/src/pages/Contactus.jsx
@@ -55,7 +55,7 @@ const Contactus = () => {
         e.preventDefault();
         setLoading(true);
         try {
-            const response = await fetch("http://localhost:5000/api/contact/send-email", {
+            const response = await fetch("https://designdeck-backend.onrender.com/api/contact/send-email", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
