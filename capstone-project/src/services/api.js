ca931e35f11833d9b0a5f106f55d6f065773962b
Harsh Vekriya <harshvekariya203@gmail.com>
2025-03-19 19:12:10 +0530

    add backend live url

--- capstone-project/src/services/api.js
+++ capstone-project/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://designdeck.onrender.com";
+const API_URL = "https://designdeck-backend.onrender.com/";
 
 export const getMessage = async () => {
     try {
