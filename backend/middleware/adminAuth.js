df971accf97ef61fd6323f9d13442455d4076910
Harsh Vekriya <harshvekariya203@gmail.com>
2025-03-23 11:47:04 +0530

    fix user login status

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,5 +1,5 @@
 const adminAuth = (req, res, next) => {
-  if (req.user && req.user.email === "harshvekriya441@gmail.com")  {
+  if (req.user && req.user.email === "harshvekriya441@gmail.com", {isAdmin: true})  {
       next(); // ✅ Admin Allowed
   } else {
       return res.status(403).json({ message: "Access Denied", isAdmin: false });
