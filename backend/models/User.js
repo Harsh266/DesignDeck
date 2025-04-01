8581cb81574ec1b0cbe406d1265d0525457a22de
Harsh Vekriya <harshvekariya203@gmail.com>
2025-04-01 12:59:24 +0530

    add default profile picture

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String }, // Optional for Google users
-    profilePicture: { type: String }, // Stores the profile picture URL
+    profilePicture: { type: String, default: "/uploads/default-profile.jpg" },// Stores the profile picture URL
     bannerImage: { type: String }, // Stores the banner image URL
     bio: { type: String, maxlength: 500 }, // User bio with a max length
     dribbbleProfile: { type: String }, // Instagram profile link
